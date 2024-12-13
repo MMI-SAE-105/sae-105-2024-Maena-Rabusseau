a7f872bed63f0b9a55b3cad37e7af07f20b1d219
Maena-Rabusseau <maena.rabusseau@edu.univ-fcomte.fr>
2024-12-13 14:56:28 +0100

    Ajout CSS + changement menu

--- src/js/script.js
+++ src/js/script.js
@@ -34,12 +34,12 @@ let currentIndex = 0;
 
 
 function updateCarousel() {
-    const translateX = -currentIndex * 100; 
+    const translateX = -currentIndex * 100;
     carousel.style.transform = `translateX(${translateX}%)`;
 }
 
 nextBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % images.length; 
+    currentIndex = (currentIndex + 1) % images.length;
     updateCarousel();
 });
 
@@ -48,6 +48,33 @@ nextBtn.addEventListener('click', () => {
 
 
 prevBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + images.length) % images.length; 
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
     updateCarousel();
+});
+
+
+
+/*lightbox*/
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const lightBox = document.querySelector("#lightbox");
+    const thumbnails = document.querySelectorAll("img[data-full-img]");
+
+    thumbnails.forEach((thumbnail) => {
+        thumbnail.addEventListener("click", () => {
+            const fullImgSrc = thumbnail.dataset.fullImg;
+            const lightBoxImg = lightBox.querySelector("img");
+            lightBoxImg.src = fullImgSrc;
+            lightBox.showModal();
+        });
+    });
+
+
+    lightBox.addEventListener("click", (event) => {
+        if (event.target === lightBox) {
+            lightBox.close();
+        }
+    });
 });
